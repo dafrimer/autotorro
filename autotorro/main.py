96c5fbc5f3ed436266e96d50960aa7008e8830d9
Daniel Frimer <danielfrimer@gmail.com>
2016-07-26 02:20:59 -0700

    initial

--- autotorro/main.py
+++ autotorro/main.py
@@ -7,20 +7,35 @@ from lxml import etree
 
 f = open('./torconfig.json')
 conf = json.load(f)
+f.close()
 
-
-
+#Mon, 01 Aug 2016 02:00:00 +0000
 def start_client():
     #try:
 
     #except:
     #    print("ERROR: there was an error reading the json configuration file")
 
-    tc = transmissionrpc.Client(address=conf['transmission_server'], port=int(conf['transmission_port']), user=conf['transmission_user'], password=conf['transmission_password'])
+    tc = transmissionrpc.Client(address=conf.pop('transmission_server'), port=int(conf.pop('transmission_port'), user=conf.pop('transmission_user'), password=conf.pop('transmission_password')
 
     return tc
 
-class
+class Torro(object):
+    def __init__(self):
+        self.tc = start_client()
+        #You need this for the raw_title
+        self.tvNamespace = "http://showrss.info"
+
+    def queryRSSFeed(self):
+        pass
+
+    def log_DLqueue(self):
+        pass
+
+    def
+
+
+
 
     d = start_client()
 
